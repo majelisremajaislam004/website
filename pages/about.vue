4a25442d945e8dfb4ba3cd087677a1c89d9c2f91
nandadianaaa <192010296@smkn4-tsm.sch.id>
2022-04-17 10:15:33 +0700

    hai

---
+++ pages/about.vue
@@ -0,0 +1,39 @@
+<template>
+    <main>
+      <div class="fixed-top">
+        <navbar/>
+      </div>
+            <div class="container" style="margin-top: 2rem;">
+                <div class="row justify-content-center" >
+                    
+                </div>
+            </div>
+        <end/>
+    </main>
+</template>
+
+<script>
+export default {
+      mounted() {
+    console.log (this.$supabase)
+  },
+  data() {
+    return {
+      his: '',
+    }
+  },
+  mounted () {
+    this.ambildata()
+  },
+  methods: {
+    async ambildata() {
+      const { data, error } = await this.$supabase
+      .from('struks')
+      .select()
+      if(data)
+      this.his = data
+      if(error) console.log(error)
+    }
+  }
+}
+</script>
---
+++ pages/activation.vue
@@ -0,0 +1,14 @@
+<template>
+    <main>
+        <div class="fixed-top">
+            <navbar/>
+        </div>
+        <div class="container" style="margin-top: 10rem;">
+            <cards/>
+        </div>
+        <div style="margin-top: 34rem;">
+            <end/>
+        </div>
+        
+    </main>
+</template>
